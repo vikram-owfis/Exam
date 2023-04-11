443bf046bf04d4512d30f31cfdc77598fd566d01
vikram-owfis <126160989+vikram-owfis@users.noreply.github.com>
2023-04-11 15:32:54 +0530

    Update Add.vue

--- components/collection/Add.vue
+++ components/collection/Add.vue
@@ -4,7 +4,7 @@
     <div class="calendar-header flex justify-between items-center mb-4">
       <button
         @click="previousMonth"
-        class="text-gray-600 px-2 py-1 border border-gray-300 rounded-md focus:outline-none ml-2"
+        class="text-gray-600 px-2 py-1 border border-gray-300 rounded-md focus:outline-none ml-2 bg-green-300 hover:bg-yellow-400"
       >
         &lt;
       </button>
@@ -31,7 +31,7 @@
       </div>
       <button
         @click="nextMonth"
-        class="text-gray-600 px-3 py-2 border border-gray-300 rounded-md focus:outline-none mr-2"
+        class="text-gray-600 px-3 py-2 border border-gray-300 rounded-md focus:outline-none mr-2 bg-green-300 hover:bg-yellow-400"
       >
         &gt;
       </button>
@@ -41,7 +41,7 @@
       <div
         v-for="day in daysInMonth"
         :key="day"
-        class="calendar-day text-gray-800 text-center flex justify-center items-center h-10 w-10 rounded-full bg-indigo-200 hover:bg-green-300"
+        class="calendar-day text-gray-800 text-center flex justify-center items-center h-10 w-10 rounded-full bg-green-200 hover:bg-green-300"
       >
         {{ day }}
       </div>
@@ -49,9 +49,9 @@
   </div>
 </template>
 
-<script setup>
+
+<script setup lang="ts">
 import {
-  ref,
   format,
   addMonths,
   subMonths,
@@ -59,40 +59,37 @@ import {
   endOfMonth,
   eachDayOfInterval,
 } from "date-fns";
+import { ref } from "vue";
 
-let currentDate = new Date();
-const years = ref([]); // Array to store years
-const months = ref([]); // Array to store months
-const selectedYear = ref(null); // Currently selected year
-const selectedMonth = ref(null); // Currently selected month
-
+const currentDate = new Date();
+const years = ref<number[]>([]);
+const months = ref<string[]>([]);
+const selectedYear = ref<number | null>(null);
+const selectedMonth = ref<number | null>(null);
 
 const currentMonth = format(currentDate, "MMMM yyyy");
-
-const daysInMonth = () => {
-  const start = startOfMonth(currentDate);
-  const end = endOfMonth(currentDate);
-  return eachDayOfInterval({ start, end }).map((date) => format(date, "dd"));
-};
+const daysInMonth = eachDayOfInterval({
+  start: startOfMonth(currentDate),
+  end: endOfMonth(currentDate),
+}).map((date) => format(date, "dd"));
 
 const nextMonth = () => {
-  currentDate = addMonths(currentDate, 1);
+  currentDate.value = addMonths(currentDate.value, 1);
   updateSelectedYearMonth();
 };
 
 const previousMonth = () => {
-  currentDate = subMonths(currentDate, 1);
+  currentDate.value = subMonths(currentDate.value, 1);
   updateSelectedYearMonth();
 };
 
 const initializeYearsAndMonths = () => {
-  // Initialize years array with current year - 10 to current year + 10
   const currentYear = new Date().getFullYear();
   for (let year = currentYear - 10; year <= currentYear + 10; year++) {
-    years.push(year);
+    years.value.push(year);
   }
-  // Initialize months array with month names
-  months = [
+
+  months.value.push(
     "January",
     "February",
     "March",
@@ -104,22 +101,23 @@ const initializeYearsAndMonths = () => {
     "September",
     "October",
     "November",
-    "December",
-  ];
-  // Set initial values for selectedYear and selectedMonth
-  selectedYear = currentYear;
-  selectedMonth = new Date().getMonth();
+    "December"
+  );
+
+  selectedYear.value = currentYear;
+  selectedMonth.value = new Date().getMonth();
 };
 
 const updateCalendar = () => {
-  const updatedDate = new Date(selectedYear, selectedMonth);
-  currentDate = updatedDate;
+  const updatedDate = new Date(selectedYear.value!, selectedMonth.value!);
+  currentDate.value = updatedDate;
 };
 
 const updateSelectedYearMonth = () => {
-  selectedYear = currentDate.getFullYear();
-  selectedMonth = currentDate.getMonth();
+  selectedYear.value = currentDate.value.getFullYear();
+  selectedMonth.value = currentDate.value.getMonth();
 };
-</script>
 
+initializeYearsAndMonths();
+</script>
 
